fe7bda68b17945502532337df2213a6156876287
Yaroslav21Savelev <37857219+Yaroslav21Savelev@users.noreply.github.com>
2019-10-25 00:44:14 +0900

    Update source.py

--- Raspberry/source.py
+++ Raspberry/source.py
@@ -112,31 +112,32 @@ def quit():
 
 def menu():
 	global process
-	tmp = 9999
-	x, y, z, mode = 0, 0, 0, 9999
+	old = 0
 	pos = 1
+	while jstk.read()[4]:
+		pass
 	while True:
 		try:
-			x, y, z, tmp = jstk.update(x, y, z, mode)
+			tmp = jstk.read()[11]
 		except:
 			quit()
-		if mode != tmp:
-			if tmp == 25:
-				return
-			elif tmp == 36:
-				pos = constrain(pos + 1, 1, 3)
-			elif tmp == 35:
-				pos = constrain(pos - 1, 1, 3)
-			elif tmp == 3 and pos == 3:
-				quit()
-			elif tmp == 3 and pos == 1:
-				#print(process.poll())
-				if not process.poll() is None:
-					process = Popen(['python', '/home/pi/host.py'])
-			elif tmp == 10 and pos == 1:
-				#print(process.poll())
-				if process.poll() is None:
+		if jstk.read()[4]:
+			return
+		if old != tmp:
+			pos = constrain(pos + tmp, 1, 3)
+			old = tmp
+		if jstk.read()[0] and pos == 1:
+			print("start streaming")
+			if not process.poll() is None:
+				process = Popen(['python', '/home/pi/host.py'])
+		if jstk.read()[3] and pos == 1:
+			print("stop streaming")
+			if process.poll() is None:
 					process.terminate()
+		elif jstk.read()[0] and pos == 2:
+			mode = 2
+		elif jstk.read()[0] and pos == 3:
+			quit()
 		dsp.clear()
 		draw.rectangle((0,0,width,height), outline=0, fill=0)
 		draw.text((0, top), "Menu", font=font, fill=255)
@@ -155,62 +156,126 @@ def menu():
 		dsp.image(image)
 		dsp.display()
 
+def info():
+	global lastTime
+	while jstk.read()[5]:
+		pass
+	while True:
+		if jstk.read()[5]:
+				return
+		if time() - lastTime > 0.5: # update display every 2 seconds
+			update_wlan_oled()
+			lastTime = time()
+		
+
+pos = 1
+old = 0
+x_old = 0
 while True:
-	try: # try to get new data from joy, except terminate process
-		x, y, z, tmp = jstk.update(x, y, z, mode)
+	try:
+		tmp = jstk.read()[11]
+		a = jstk.read()[0]
 	except:
 		quit()
-	if time() - lastTime > 2: # update display every 2 seconds
-		update_wlan_oled()
-		lastTime = time()
-	if mode != tmp:
-		if tmp == 10: #attach/detach
-			attached = not attached
-			if attached:
-				do.attach_all()
-			else:
-				for i in range(18):
-					servo.detach(i)
-		elif tmp == 30:
-			servo.holder(0)
-		elif tmp == 31:
-			servo.holder(1)
-		elif tmp == 4:
-			mode = tmp
-			x_c, y_c, z_c = 5, 0, -6
-			s_speed = 40
-			fr.move(3, 7, -3, s_speed)
-			fl.move(3, 7, -3, s_speed)
-			mr.move(x_c + 1, y_c + 1, z_c - 3.5, s_speed)
-			ml.move(x_c + 2, y_c + 1, z_c - 3.5, s_speed)
-			br.move(x_c - 0, y_c - 6, z_c + 1.8, s_speed)
-			bl.move(x_c - 0, y_c - 6, z_c + 1.8, s_speed)
-		elif tmp == 25:
-			tmp = 0
-			menu()
+	if jstk.read()[8]:
+		servo.holder(0)
+	elif jstk.read()[9]:
+		servo.holder(1)
+			
+	if jstk.read()[4]:
+		menu()
+	while jstk.read()[4]:
+		pass
+	if jstk.read()[5]:
+		info()
+	while jstk.read()[5]:
+		pass
+	x = jstk.read()[3]
+	if x and x_old == 0:
+		attached = not attached
+		if attached:
+			do.attach_all()
 		else:
-			mode = tmp
-		#pos = 0
+			for i in range(18):
+				servo.detach(i)
+		x_old = 1
+	if not x:
+		x_old = 0
+	if old != tmp:
+		print(pos, tmp, a)
+		pos = constrain(pos + tmp, 1, 4)
+		old = tmp
+	if a and pos == 1 and mode != 1:
+		mode = 1
+	elif a and pos == 2 and mode != 2:
+		mode = 2
+	elif a and pos == 3 and mode != 3:
+		mode = 3
+	elif a and pos == 4 and mode != 4:
+		x_c, y_c, z_c = 5, 0, -6
+		s_speed = 40
+		fr.move(3, 7, -3, s_speed)
+		fl.move(3, 7, -3, s_speed)
+		mr.move(x_c + 1, y_c + 1, z_c - 3.5, s_speed)
+		ml.move(x_c + 2, y_c + 1, z_c - 3.5, s_speed)
+		br.move(x_c - 0, y_c - 6, z_c + 1.8, s_speed)
+		bl.move(x_c - 0, y_c - 6, z_c + 1.8, s_speed)
+		mode = 4
+	dsp.clear()
+	draw.rectangle((0,0,width,height), outline=0, fill=0)
+	if attached:
+		draw.text((0, top), "Attached!", font=font, fill=255)
+	else:
+		draw.text((0, top), "Detached", font=font, fill=255)
+	if pos == 1:
+		draw.text((0, top + 8), "", font=font, fill=255)
+		draw.text((0, top + 16), ">Walk", font=font, fill=255)
+		draw.text((0, top + 24), "Agile", font=font, fill=255)
+	elif pos == 2:
+		draw.text((0, top + 8), "Walk", font=font, fill=255)
+		draw.text((0, top + 16), ">Agile", font=font, fill=255)
+		draw.text((0, top + 24), "Flex", font=font, fill=255)
+	elif pos == 3:
+		draw.text((0, top + 8), "Agile", font=font, fill=255)
+		draw.text((0, top + 16), ">Flex", font=font, fill=255)
+		draw.text((0, top + 24), "Tarantul", font=font, fill=255)
+	elif pos == 4:
+		draw.text((0, top + 8), "Flex", font=font, fill=255)
+		draw.text((0, top + 16), ">Tarantul", font=font, fill=255)
+		draw.text((0, top + 24), "", font=font, fill=255)
+	dsp.image(image)
+	dsp.display()
 	if mode == 1:
-		#print(x, y)
+		x = jstk.read()[13]
+		y = jstk.read()[12]
 		if abs(x) >= 0.5 or abs(y) >= 0.5:
 			do.go(-y, -x * 1.2, z)
 		elif (x_ != x or y_ != y or z_ != z):
 			do.go(0, 0, z, 1)
 			x_, y_, z_ = x, y, z
 	elif mode == 2:
+		x = jstk.read()[13]
+		y = jstk.read()[12]
+		z = jstk.read()[17]
 		if(x_ != x or y_ != y or z_ != z):
 			do.agile(x, y, z)
 			x_, y_, z_ = x, y, z
 	elif mode == 3:
+		x = jstk.read()[13]
+		y = jstk.read()[12]
+		z = jstk.read()[17]
 		if(x_ != x or y_ != y or z_ != z):
 			do.flex(x, y, z)
 			x_, y_, z_ = x, y, z
 	elif mode == 4:
-		if(x_ != x or y_ != y or z_ != z):
-			fr.move(4 + x * 0.8, 8.5 - z * 0.8, -5 - y, 250)
-			fl.move(3, 7, -3, 250)
-			x_, y_, z_ = x, y, z
+		x_l = jstk.read()[14]
+		y_l = jstk.read()[15]
+		x_r = jstk.read()[13]
+		y_r = jstk.read()[12]
+		z_l = jstk.read()[16]
+		z_r = jstk.read()[17]
+		fr.move(4 + x_r * 0.8, 8.5 - z_r * 0.8, -5 - y_r, 250)
+		fl.move(4 + x_l * 0.8, 8.5 - z_l * 0.8, -5 - y_l, 250)
 	
 
 
